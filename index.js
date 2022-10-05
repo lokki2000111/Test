ecbd0d4d986816097d30418b5123df85b164bd9d
lokki2000111 <75306173+lokki2000111@users.noreply.github.com>
2022-10-05 14:10:14 +0300

    Create index.js
    Test

---
+++ index.js
@@ -0,0 +1,12 @@
+function countSheeps(arrayOfSheep) {
+  const arr = arrayOfSheep;
+  let count = 0;
+  for(let i = 0 ; i <= arr.length ; i++){
+    if(arr[i] === true){
+      count++;
+    }else{
+      continue;
+    }
+  }
+  console.log(count);;
+}
